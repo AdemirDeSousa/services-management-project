a6bfe6cb470779b457f8aa306e23363a87209f01
AdemirDeSousa <jadsqj@gmail.com>
2020-08-24 11:43:10 -0300

    Iniciando o back-end do projeto

---
+++ services-back-end/src/main/java/com/ademir/ServicesBackEndApplication.java
@@ -0,0 +1,15 @@
+package com.ademir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesBackEndApplication.class, args);
+		
+		//iau
+	}
+
+}
