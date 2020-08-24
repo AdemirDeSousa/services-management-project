3e733b7fdc893a5c18cb82da2ec73338c79cb1ce
AdemirDeSousa <jadsqj@gmail.com>
2020-08-24 12:11:51 -0300

    Mapeando entidade servico

---
+++ services-back-end/src/main/java/com/ademir/model/entities/Service.java
@@ -0,0 +1,32 @@
+package com.ademir.model.entities;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Service {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, length = 150)
+	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private Client cliente;
+	
+	@Column
+	private BigDecimal value;
+}
